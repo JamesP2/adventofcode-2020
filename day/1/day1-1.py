bb7b786e63b9e0a302bc177d1936fd95276e31bb
James Phillips <jamesp2.phillips@gmail.com>
2020-12-13 00:05:02 +0000

    Initial Commit

---
+++ day/1/day1-1.py
@@ -0,0 +1,12 @@
+numbers = []
+
+with open('input') as file:
+    for line in file:
+        numbers.append(int(line))
+
+for first in numbers:
+    for second in numbers:
+        if first + second == 2020:
+            product = first * second
+            print(
+                '{} + {} = 2020. The product of these numbers is {}'.format(first, second, product))
