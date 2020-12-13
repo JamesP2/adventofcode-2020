2200604ea8e606ca6330804a799b00d2f89b515c
James Phillips <jamesp2.phillips@gmail.com>
2020-12-13 00:47:24 +0000

    2.2

---
+++ day/2/day2-2.py
@@ -0,0 +1,27 @@
+import re
+
+valid_passwords = 0
+
+with open('input') as file:
+    for line in file:
+        # Almost did line[:-2] but then the last line would be too short
+        line = line.replace('\n', '')
+
+        # Split line into [minimum, maximum, char, password]
+        first_index, second_index, char, password = re.split(
+            '-| |: ', line)
+
+        first_index = int(first_index) - 1
+        second_index = int(second_index) - 1
+
+        valid = True if (password[first_index] == char and password[second_index] != char) or (
+            password[first_index] != char and password[second_index] == char) else False
+
+        print('Looking for {} in password {}. Char {} = {}, Char {} = {}, {}'.format(
+            char, password, first_index, password[first_index], second_index, password[second_index], 'Valid' if valid else 'Not Valid!'))
+
+        if valid:
+            valid_passwords += 1
+
+
+print('There are {} valid passwords in the data set.'.format(valid_passwords))
